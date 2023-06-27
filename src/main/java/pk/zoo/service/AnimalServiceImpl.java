69894a442e8b17017772a30073f7aaac31ff7da9
Kopecký Petr <petr.kopecky@t-mobile.cz>
2023-06-27 19:30:09 +0200

    animal3

--- src/main/java/pk/zoo/service/AnimalServiceImpl.java
+++ src/main/java/pk/zoo/service/AnimalServiceImpl.java
@@ -3,22 +3,23 @@ package pk.zoo.service;
 import pk.zoo.model.Animal;
 import pk.zoo.model.Employee;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class AnimalServiceImpl implements AnimalService{
-    Map<Long,Animal> animals;
+public class AnimalServiceImpl implements AnimalService {
+    Map<Long, Animal> animals;
     Map<Long, List<Employee>> animalsCarers;
 
-    public void  init(){
-        animals= new HashMap<Long,Animal>();
-        animalsCarers =new HashMap<Long,List<Employee>>();
+    public void init() {
+        animals = new HashMap<Long, Animal>();
+        animalsCarers = new HashMap<Long, List<Employee>>();
     }
 
     @Override
-    public void addAnimal(Animal animal,List<Employee> caredByList) {
-        animals.put(animal.getAin(),animal);
+    public void addAnimal(Animal animal, List<Employee> caredByList) {
+        animals.put(animal.getAin(), animal);
         animalsCarers.put(animal.getAin(), caredByList);
 
     }
@@ -31,6 +32,15 @@ public class AnimalServiceImpl implements AnimalService{
 
     @Override
     public void addCaredByToAnimal(Employee employee, Animal animal) {
+        List<Employee> carers = animalsCarers.get(animal.getAin());
+
+        if (carers == null) {
+            List<Employee> carersNew = new ArrayList<Employee>();
+            carersNew.add(employee);
+            animalsCarers.put(animal.getAin(), carersNew);
+        }else {
+            carers.add(employee);
+        }
 
     }
 }
